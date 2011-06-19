9de2162f295a415476995c7a47faaebdf46924c5
征矢 直記 <soyanaoki@susumuya-naoki-no-MacBook-Air.local>
2011-06-19 15:50:46 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railbook::Application.config.secret_token = '59729c009925d66a5da05f81234d990628cfc297e6895c728e1ba25492d09b8de65657c9033730a7f9fd941984fa2c8b7a208c05657e7e161dd97dc3d38adaa4'
